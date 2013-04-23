f24f992004e0f99f1da9a9796c7443aae400bc06
Michelle Lowe <mick_571@yahoo.com>
2013-04-22 21:35:46 -0700

    Addl styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    session[:previous_url] || bookbag_path
   end
   
 end
