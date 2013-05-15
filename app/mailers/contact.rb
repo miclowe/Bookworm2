29b9d3ff59d0f815be9f0bf1cc5dc1a9a651a18d
Michelle Lowe <mick_571@yahoo.com>
2013-05-14 17:56:36 -0700

    Final updates

--- app/mailers/contact.rb
+++
@@ -1,9 +0,0 @@
-class Contact < ActionMailer::Base
-  default to: ENV["GMAIL_USERNAME"]
-
-  def form_email(from, subject, body)
-    @body = body
-    mail(:from => from, :subject => subject)
-  end
-
-end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,20 @@ Bookworm2::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  # Delivery method
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "cssdash.com",
+    :user_name => "mklowe2012",
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+  }
 
   # Enable threaded mode
   # config.threadsafe!
