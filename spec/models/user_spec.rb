4c0792ad46570f80668850c7006a96b637ac0f06
Michelle Lowe <mick_571@yahoo.com>
2013-05-14 22:18:16 -0700

    Refactor user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,28 @@ require 'spec_helper'
 
 describe User do
 
+  before(:each) do
+    @user = User.new
+    @user.first_name = "Michelle"
+    @user.last_name = "Lowe"
+    @user.email = "mklowe2012@gmail.com"
+    @user.password = "secret"
+  end
+
   it "has a first name" do
-    user = User.new(:first_name => "Michelle")
-    expect(user.first_name).to eq "Michelle"
+    expect(@user.first_name).to eq "Michelle"
   end
 
   it "has a last name" do
-    user = User.new(:last_name => "Lowe")
-    expect(user.last_name).to eq "Lowe"
+    expect(@user.last_name).to eq "Lowe"
   end
 
   it "has an email" do
-    user = User.new(:email => "mklowe2012@gmail.com")
-    expect(user.email).to eq "mklowe2012@gmail.com"
+    expect(@user.email).to eq "mklowe2012@gmail.com"
   end
 
   it "has a password" do
-    user = User.new(:password => "testpassword")
-    expect(user.password).to eq "testpassword"
+    expect(@user.password).to eq "secret"
   end
 
 end
