b7f7882827e54aa6a481534aedff7d2cff0446f7
Michelle Lowe <mick_571@yahoo.com>
2013-07-13 13:37:46 -0700

    Mailer working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,11 +19,12 @@ Bookworm2::Application.configure do
   # Delivery method
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "bookworm.com",
-    :user_name => "mklowe2012",
+    :domain => "gmail.com",
+    :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,11 +54,12 @@ Bookworm2::Application.configure do
   # Delivery method
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'bookworm2.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "cssdash.com",
-    :user_name => "mklowe2012",
+    :domain => "gmail.com",
+    :user_name => ENV["GMAIL_USERNAME"],
     :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true,
